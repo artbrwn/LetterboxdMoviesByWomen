3b2a65f3683e049813af6864e15378d49dab3163
Arturo <md.arturo@gmail.com>
2025-05-31 22:36:01 +0200

    Fully functional app

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 import os
 import json
+import time
 
 # API
 load_dotenv()
@@ -21,17 +22,42 @@ with open("data/watched.csv", "r") as csv_file:
 
 # PROCESS DATA
 def get_movie_id_from_lett(lett_movie):
-    return "835504"
+    """
+    It gets a tupple (title, year) and it returns the tmdb id of that movie.
+    """
+    movie_response = requests.get(f"{TMDB_BASE_URL}/search/movie?query={lett_movie[0]}&year={lett_movie[1]}&api_key={TMDB_API_KEY}")
+    if movie_response.status_code == 429:
+        print("Rate limit exceeded, sleeping 10 seconds")
+        time.sleep(10)
+    movie_json = json.loads(movie_response.text)
+    if "results" in movie_json and len(movie_json) > 0: # Manages in case no results are found
+        movie_id = movie_json["results"][0]["id"]
+        return movie_id
+    else:
+        print(f"No results for movie: {lett_movie}")
+        return None
+
+def get_gender_from_movie(movie_id):
+    """
+    It gets an id of a movie and returns True if at least one of the directors is a woman
+    """
+    movie_response = requests.get(f"{TMDB_BASE_URL}/movie/{movie_id}/credits?api_key={TMDB_API_KEY}")
+
+    movie_json = json.loads(movie_response.text)
+    if movie_response.status_code == 429:
+        print("Rate limit exceeded, sleeping 10 seconds")
+        time.sleep(10)
+    directors = [member for member in movie_json["crew"] if member["job"] == "Director"]
+    directors_genders = [director["gender"] for director in directors]
+    return directors_genders
 
 def is_directed_by_woman(lett_movie): # PRUEBA, CAMBIAR NOMBRE A  is_directed_by_woman
     # get the ID of the movie from lett_movie
     movie_id = get_movie_id_from_lett(lett_movie)
+    if movie_id is None: # In case get_movie_id_from_lett returns None for a fail
+        return False
     # search the movie in tmdb
-    movie_response = requests.get(f"{TMDB_BASE_URL}/movie/{movie_id}/credits?api_key={TMDB_API_KEY}")
-    movie_json = json.loads(movie_response.text)
-
-    directors = [member for member in movie_json["crew"] if member["job"] == "Director"]
-    directors_genders = [director["gender"] for director in directors]
+    directors_genders = get_gender_from_movie(movie_id)
     # if it has one director
     if len(directors_genders) == 1:
         # if its a woman
@@ -45,19 +71,21 @@ def is_directed_by_woman(lett_movie): # PRUEBA, CAMBIAR NOMBRE A  is_directed_by
         # if one is a woman
         return any(gender == 1 for gender in directors_genders)
 
-    
-    # If nothing happens return False
-
 # Iterates watched_movies, applies is_directed_by_woman() and appends to watched_movies_by_women if True.
+n_of_movies_total = len(watched_movies)
 watched_movies_by_women = []
 
 for movie in watched_movies:
     if is_directed_by_woman(movie):
         watched_movies_by_women.append(movie)
+    
+    print(f"Analyzing {watched_movies.index(movie)} of {n_of_movies_total}")
+    time.sleep(0.05)
 
-n_of_movies_total = len(watched_movies)
 n_of_movies_by_women = len(watched_movies_by_women)
 percentage_by_women = (n_of_movies_by_women *100) / n_of_movies_total
 
+
 # PRINT RESULTS
-print(f"You've watched {n_of_movies_by_women} movies directed by women out of {n_of_movies_total} movies in total. That is a {percentage_by_women}%.")
+print(f"You've watched {n_of_movies_by_women} movies directed by women out of {n_of_movies_total} movies in total. That is a {percentage_by_women:.2f}%.")
+
