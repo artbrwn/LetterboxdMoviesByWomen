1ebcc92324c649ac440361628c8df5e27c72af17
Arturo <md.arturo@gmail.com>
2025-06-05 17:32:05 +0200

    updated tests for Movie class, updated .gitignore file

--- movie_test.py
+++ movie_test.py
@@ -1,6 +1,8 @@
 from movie import Movie
 
 def test_movie_creation():
-    titanic = Movie("Titanic", 1997)
+    titanic = Movie("Titanic", "1997", "https://boxd.it/2a2k")
     assert titanic.title == "Titanic"
-    assert titanic.year == 1997
+    assert titanic.year == "1997"
+    titanic.fetch_tmdb_id()
+    assert titanic.tmdb_id == 597
